5dab054923688382866b37293c93fdd6777db93c
Aqeedat-Nabi <2023_aqeedat.ghulam@iticbcn.cat>
2024-01-09 20:58:40 +0100

    1st

---
+++ REVIEW/exercise3.py
@@ -0,0 +1,6 @@
+first = int(input("Enter a number : \n"))
+second = int(input("Enter a second number : \n"))
+if (first > second):
+    print("The greater number is : " + str(first))
+else:
+    print("The greater number is : " + str(second))
